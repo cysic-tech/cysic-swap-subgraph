3ca20ae04825ed73ed190f7d40c8370797ab479e
Eden <eden.cc@outlook.com>
2025-02-18 03:15:06 +0800

    feat: logs

--- subgraphs/exchange-v3/mappings/core.ts
+++ subgraphs/exchange-v3/mappings/core.ts
@@ -153,8 +153,8 @@ export function handleMint(event: MintEvent): void {
   let lowerTickId = `${poolAddress}#${BigInt.fromI32(event.params.tickLower).toString()}`
   let upperTickId = `${poolAddress}#${BigInt.fromI32(event.params.tickUpper).toString()}`
 
-  log.info('Lower Tick ID: {}', [lowerTickId.toString()]);
-  log.info('Upper Tick ID: {}', [upperTickId.toString()]);
+  // log.info('Lower Tick ID: {}', [lowerTickId.toString()]);
+  // log.info('Upper Tick ID: {}', [upperTickId.toString()]);
 
   let lowerTick = Tick.load(lowerTickId)
   let upperTick = Tick.load(upperTickId)
