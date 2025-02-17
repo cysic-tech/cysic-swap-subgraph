fc0cb5a7b30fca075b3f835b1cf07a36f4544c67
Eden <eden.cc@outlook.com>
2025-02-18 02:41:42 +0800

    feat: logs

--- subgraphs/exchange-v3/mappings/core.ts
+++ subgraphs/exchange-v3/mappings/core.ts
@@ -578,7 +578,7 @@ function updateTickFeeVarsAndSave(tick: Tick, event: ethereum.Event): void {
   tick.feeGrowthOutside0X128 = tickResult.value2
   tick.feeGrowthOutside1X128 = tickResult.value3
 
-  log.info('tick ready to save, poolAddress = {}, tickIdx = {}', [tick.poolAddress.toString(), tick.tickIdx.toString()])
+  // log.info('tick ready to save, poolAddress = {}, tickIdx = {}', [tick.poolAddress.toString(), tick.tickIdx.toString()])
   tick.save()
   // log.info('tick save: {}', [tick.tickIdx.toString()]);
 
